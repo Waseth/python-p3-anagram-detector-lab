514363ae6ebb1d56fbfe223cf24af63ca304e52e
Waseth <waseth.emmanuel@student.moringaschool.com>
2024-06-10 11:27:45 +0300

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
-# your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, candidates):
+        anagrams = []
+        sorted_word = sorted(self.word)
+
+        for candidate in candidates:
+            candidate_lower = candidate.lower()
+            if candidate_lower != self.word and sorted(candidate_lower) == sorted_word:
+                anagrams.append(candidate)
+
+        return anagrams
